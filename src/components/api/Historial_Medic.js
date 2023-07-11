c13f09afef8b32b250be92db02e8acd5a700c0f7
carloslorap <74977007@certus.edu.pe>
2023-07-11 01:42:20 -0500

    wef

--- src/components/api/Historial_Medic.js
+++ src/components/api/Historial_Medic.js
@@ -1,16 +1,19 @@
 import axios from "axios";
-
+import { toast } from "react-toastify";
 export const HistorialMedic=(his)=>{
  
-    axios.post('https://smarth-resultdado-service.azurewebsites.net/resultado', his)
+    axios.post('https://smarth-resultado-service.azurewebsites.net/resultado', his)
     .then(response => {
       console.log(response.data); 
       // handle success response
+      toast.success('Bien') 
     })
     .catch(error => {
       console.log(error.response.data);
       // handle error response
+      toast.error(' Failed')
     });
 }
+
 
 //https://smarth-resultdado-service.up.railway.app/resultado
